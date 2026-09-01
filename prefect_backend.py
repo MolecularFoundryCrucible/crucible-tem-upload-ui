44e271c12387897475c9b7ffb1cb518ba662f334
Morgan Wall <mkywall3@gmail.com>
2026-08-31 17:26:01 -0700

    nanocruc 3.2

--- prefect_backend.py
+++ prefect_backend.py
@@ -22,7 +22,7 @@ class MultipleSessionsFound(Exception):
 
 
 try:
-    client = CrucibleClient(api_url = 'https://crucible.lbl.gov/api/v2')
+    client = CrucibleClient(api_url = 'https://crucible.lbl.gov/3')
     assert client.api_key is not None
     logger.info(f'Connected to Crucible Client with API url: {client.api_url}')
 
